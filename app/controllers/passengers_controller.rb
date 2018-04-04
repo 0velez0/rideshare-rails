5d84e9cfc16b5cbae8edc2d7b6d7ecc71f20be23
0velez0 <mariaivelez@gmail.com>
2018-04-03 17:40:14 -0700

    create passenger controller, index view and method

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,26 @@
+class PassengersController < ApplicationController
+
+def index
+  @passengers = Passenger.all
+end
+
+def create
+
+end
+
+def new
+end
+
+def show
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :passengers
 
-  resources :trips 
+  # we don't need a list of all the trips, we don't need functionality to create a new trip
+  resources :trips, except: [:index, :new]
 
 end
