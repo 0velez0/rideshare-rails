00f58cfadbbd75e43164bb28ee75ed034fcf5519
Samantha Berk <sberk10@gmail.com>
2018-04-04 17:07:37 -0700

    show list of trips on passenger page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  resources :drivers
-
-  resources :passengers
-
+  resources :drivers do
+    resources :trips, only: [:index, :new, :create]
+  end
+  
+  resources :passengers do
+    resources :trips, only: [:index, :new, :create]
+  end
   # we don't need a list of all the trips, we don't need functionality to create a new trip
   resources :trips, except: [:index, :new]
 
