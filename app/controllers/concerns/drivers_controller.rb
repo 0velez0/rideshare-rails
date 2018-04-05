dd27b69c4c7ef333704796fa4e89387502986fe6
0velez0 <mariaivelez@gmail.com>
2018-04-05 15:04:42 -0700

    create form partial for driver, add edit function to driver

---
+++ app/controllers/concerns/drivers_controller.rb
@@ -0,0 +1,43 @@
+class DriversController < ApplicationController
+
+  def index
+    @drivers = Driver.all
+  end
+
+  def create
+
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def show
+    @driver = Driver.find(params[:id])
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    driver = Driver.find(params[:id])
+    driver.assign_attributes(driver_params)
+    if driver.save
+      redirect_to driver_path(driver.id)
+    else
+      render :edit 
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+
+
+end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,13 +3,13 @@ class Driver < ApplicationRecord
 
   def total_revenue
     fee = 1.65
-    subtotal = 0 
+    subtotal = 0
     driver_take_home = 0.8
 
     self.trips.each do |trip|
       subtotal += trip.cost - fee
     end
-    total = subtotal * driver_take_home
+    total = (subtotal * driver_take_home).round(2)
     return total
   end
 
@@ -18,11 +18,13 @@ class Driver < ApplicationRecord
     self.trips.each do |trip|
       total_ratings += trip.rating
     end
-    average = (total_ratings.to_f) / trips.length
-
+    average = ((total_ratings.to_f) / trips.length).round(2)
     return average
   end
 
+  # def car_make
+  #
+  # end 
 
 
 end
