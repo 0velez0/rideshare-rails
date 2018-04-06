5f16c12a79efedf687bbee4443e1531cd85aeb38
Samantha Berk <sberk10@gmail.com>
2018-04-06 14:20:00 -0700

    validations for adding/editing passengers

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :trips, dependent: :destroy
 
+  validates :name, presence: true, format: {with: /\A[a-zA-Z\s\.]+\z/}
+  validates :phone_num, presence: true
+
 end
