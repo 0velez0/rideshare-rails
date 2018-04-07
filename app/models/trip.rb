e2613879c51c717d47cd76263f639e8e31c684b4
Samantha Berk <sberk10@gmail.com>
2018-04-06 17:00:53 -0700

    only accept 0-5 for trip rating

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
-  validates :rating, allow_nil: true, numericality: { only_integer: true, less_than_or_equal_to: 5 }
 
   def self.create_trip(passenger)
     # in charge of finding a driver and calling Trip.new or Trip.create to assoc trip with it
